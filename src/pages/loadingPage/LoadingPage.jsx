832978e6ba97414b4155475c994e5d37abae1f82
IgorMazhitov <101721767+IgorMazhitov@users.noreply.github.com>
2022-12-08 04:16:36 +0300

    changes in loading screen

--- src/pages/loadingPage/LoadingPage.jsx
+++ src/pages/loadingPage/LoadingPage.jsx
@@ -1,8 +1,20 @@
     
     import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
     import { ReactComponent as Background } from "../../assets/anim_bg.svg"
 
         const LoadingPage = (props) => {
+
+            const [slideAnim, setSlideAnim] = useState('duration-1000 absolute w-full h-fit flex justify-start pl-10 items-center translate-x-1/2 opacity-0 top-[23%] left-0 mix-blend-difference')
+
+            useEffect(() => {
+
+                setSlideAnim(prev => prev.replace('opacity-0', 'opacity-100'))
+                setTimeout(() => {
+                    setSlideAnim(prev => prev.replace(' translate-x-1/2', ' -translate-x-1/2'))
+                }, 1500)
+            }, [])
 
             return (
 
@@ -21,7 +33,7 @@
                         </g>
                     </svg>
 
-                    <svg className="absolute w-full h-fit flex justify-start pl-10 items-center -translate-x-1/2 top-[23%] left-0">
+                    <svg id="svg_front_slide" className={slideAnim}>
                         <symbol id="text-copy2" >
                             <text text-anchor="middle2" x="50%" y="50%" class="text--line2 text-[50px] font-bold">FRONT END DEVELOPER</text>
                         </symbol>
